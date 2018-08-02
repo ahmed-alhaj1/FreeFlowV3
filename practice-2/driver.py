7bbcc33df18d001bfedbfff55ce8d0d7eb5dcec2
TadewosBell <bell6@umbc.edu>
2018-08-02 00:28:20 -0400

    login session is tracked

--- practice-2/driver.py
+++ practice-2/driver.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, render_template, flash, redirect, url_for, abort, g, session, request
 from config import Config
 from flask_script import Manager
+from flask import session
 # from flask_dropzone import Dropzone
 #from flask_bcrypt import check_password_hash
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
@@ -13,7 +14,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.config['SECURITY_KEY'] = 'secret'
+app.secret_key = 'xc9*\x8f\xd1Z\x0e_;Z\x19XqK\x8fW\x01\xf1\xf1\xe2\x12V\xac'
 app.config['DEBUG'] = True
 app.config['ALLOWED_EXTENSIONS'] = set(["pdf", "docx", "doc"])
 app.config.update(
@@ -35,10 +36,11 @@ def create_db():
 @app.route('/')
 def home():
 	from models import User
-	if user == '':
+	userName = session['userName']
+	if userName== None:
 		return render_template('home.html')
 	else:
-		return "Welcome to FreeFlow " + user
+		return "Welcome to FreeFlow " + userName
 
 @app.route('/options', methods=['GET','POST'])
 def options():
@@ -61,14 +63,14 @@ def login():
 def load_login():
 	from models import User
 	if request.method == 'POST':
-		print("here0")
-		username = request.form['username']
-		owner = User.query.filter_by(username=username).first()
+		# print("here0")
+		userName = request.form['username']
+		owner = User.query.filter_by(username=userName).first()
 		if owner != None:
 			print("here1")
 			if owner.get_password() == request.form['password']:
-				user = username
-				return render_template('upload.html', user = user)
+				session['userName'] = userName
+				return render_template('upload.html', user = userName)
 
 			else:
 				return render_template('login.html', message = 'Wrong Password')
@@ -93,27 +95,28 @@ def create_user():
 
 @app.route('/uploads')
 def pre_upload():
-	if user == '':
+	if 'userName' not in session:
 		return render_template('login.html')
 	else:
+		user = session["userName"]
 		return render_template('upload.html', user = user)
 
 @app.route('/upload_file', methods=['POST','GET'])
 def upload_file():
 	from models import FileContents, User
-
+	user = session["userName"]
 		# owner = User.query.filter_by(username=user).first()
 		#print(owner.id , owner.username)
 	if request.method == 'POST':
 
 		input_file = request.files['file']
 			#print(owner.id , owner.username)
-		owner = User.query.filter_by(user).first()
+		owner = User.query.filter_by(username=user).first()
 		new_file = FileContents(data=input_file.read(), filename=input_file.filename, user_id =owner.username)
 		#db.create_all()
 		db.session.add(new_file)
 		db.session.commit()
-		return (input_file.filename, "this file has been upload successfully")
+		return (input_file.filename + " this file has been upload successfully")
 
 
 
